bb291a751d12397078906867ab3e66b13ead9d08
Sarah Marshall <sarah@sarahmarshall.name>
2021-02-28 23:40:27 -0800

    Use hspec-parsec

--- test/Kombucha/Test/ParserSpec.hs
+++ test/Kombucha/Test/ParserSpec.hs
@@ -4,10 +4,16 @@ import Kombucha.Parser
 import Kombucha.SyntaxTree
 import Kombucha.TwoOrMore
 import Test.Hspec
+import Test.Hspec.Parsec
 import Text.Parsec
 
 spec :: Spec
 spec = describe "parser" $ do
   it "parses parameter declarations" $ do
-    parse parameterSpec "" "parameter Party = Alice | Bob"
-      `shouldBe` Right ParameterSpec {name = "Party", values = ("Alice", "Bob") ::| []}
+    let parameterSpec' = parse parameterSpec ""
+
+    parameterSpec' "parameter Party = Alice | Bob"
+      `shouldParse` ParameterSpec
+        { name = "Party",
+          values = ("Alice", "Bob") ::| []
+        }
