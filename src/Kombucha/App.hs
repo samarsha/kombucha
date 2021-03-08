f6ff4f5b034515e558335f90e215344463c92ce7
rsoiffer <rory.soiffer@comcast.net>
2021-03-07 17:32:58 -0800

    Added prettyprinting to output

--- src/Kombucha/App.hs
+++ src/Kombucha/App.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Kombucha.App (main) where
 
+import Data.Function
+import Data.List
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Kombucha.SyntaxTree
+import qualified Kombucha.TwoOrMore as TwoOrMore
 import Kombucha.Verifier
 import System.Environment
 import System.Exit
@@ -16,7 +24,31 @@ main = do
         Left err -> do
           pHPrint stderr err
           exitFailure
-        Right predicates -> pPrint predicates
+        Right predicates -> putStrLn (prettyPrint predicates)
     _ -> do
       hPutStrLn stderr "Usage: kombucha <filename>"
       exitFailure
+
+class PrettyPrintable a where
+  prettyPrint :: a -> String
+
+instance PrettyPrintable (Map Name [Predicate]) where
+  prettyPrint m = Map.toList m & map prettyPrint & intercalate "\n\n"
+
+instance PrettyPrintable (Name, [Predicate]) where
+  prettyPrint (name, pred) = name ++ " is valid when:" ++ (pred & map prettyPrint & map (\s -> "\n" ++ s) & concat)
+
+instance PrettyPrintable Predicate where
+  prettyPrint (IsResource type') = prettyPrint type' ++ " is Resource"
+  prettyPrint (IsParam type' name) = prettyPrint type' ++ " is " ++ name
+
+instance PrettyPrintable Type where
+  prettyPrint (TypeInference (type1 :|- type2)) = prettyPrint type1 ++ " |- " ++ prettyPrint type2
+  prettyPrint (TypeResource resource) = prettyPrint resource
+  prettyPrint (TypeParam name) = name
+  prettyPrint (TypeVariable name) = name
+
+instance PrettyPrintable Resource where
+  prettyPrint ResourceUnit = "0"
+  prettyPrint (ResourceAtom name types) = name ++ (types & map prettyPrint & map (\s -> " " ++ s) & concat)
+  prettyPrint (ResourceTuple types) = "(" ++ (types & TwoOrMore.toList & map prettyPrint & intercalate ", ") ++ ")"
